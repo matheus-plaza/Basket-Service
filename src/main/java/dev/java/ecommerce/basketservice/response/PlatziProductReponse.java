3139bbf51d1879323d3577735353596d27bb7417
Matheus Plaza <plazachave.mp7@gmail.com>
2025-11-22 21:48:11 -0300

    feat: implement cache for product service

--- src/main/java/dev/java/ecommerce/basketservice/response/PlatziProductReponse.java
+++ src/main/java/dev/java/ecommerce/basketservice/response/PlatziProductReponse.java
@@ -1,8 +1,9 @@
 package dev.java.ecommerce.basketservice.response;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 public record PlatziProductReponse(Long id,
                                    String title,
-                                   BigDecimal price) {
+                                   BigDecimal price) implements Serializable {
 }
--- src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
@@ -3,21 +3,27 @@ package dev.java.ecommerce.basketservice.service;
 import dev.java.ecommerce.basketservice.client.PlatziStoreClient;
 import dev.java.ecommerce.basketservice.response.PlatziProductReponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final PlatziStoreClient storeClient;
 
+    @Cacheable(value = "products")
     public List<PlatziProductReponse> getAllProducts(){
+        log.info("Getting all products");
         return storeClient.getAllProducts();
     }
 
-    public PlatziProductReponse getProduct(Long id){
-        return storeClient.getProduct(id);
+    @Cacheable(value = "product", key = "#productId")
+    public PlatziProductReponse getProduct(Long productId){
+        log.info("Getting product with id {}", productId);
+        return storeClient.getProduct(productId);
     }
 }
